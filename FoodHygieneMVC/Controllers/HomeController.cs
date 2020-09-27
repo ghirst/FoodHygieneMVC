fab3a90347dd57819fb9594711643e4abfeb725a
Indy <hirst.geoffrey@gmail.com>
2020-09-27 22:05:50 +0100

    Working on Establishments?localAuthorityId=760 remember to change back to the authID

--- FoodHygieneMVC/Controllers/HomeController.cs
+++ FoodHygieneMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace FoodHygieneMVC.Controllers
@@ -30,7 +31,7 @@ namespace FoodHygieneMVC.Controllers
             client.DefaultRequestHeaders.Add("x-api-version", "2");
 
             //Sending request to find web api REST service resource GETRegions using HttpClient
-            HttpResponseMessage Res = await client.GetAsync("Authorities/basic");
+            HttpResponseMessage Res = await client.GetAsync("Authorities/basic");  
 
             //Checking the response is successful or not which is sent using HttpClient
             if (Res.IsSuccessStatusCode)
@@ -43,13 +44,36 @@ namespace FoodHygieneMVC.Controllers
                 AuthInfo = dataArray.Authorities;
 
                 var distinctLocalAuthorityIDCodeList = AuthInfo.Select(o => o.LocalAuthorityIdCode).Distinct().ToList();
-                 
-               
+
+                var StarExcept = 0;
+                var Star5 = 0;
+                var Star4 = 0;
+                var Star3 = 0;
+                var Star2 = 0;
+                var Star1 = 0;
 
                 foreach (var authID in distinctLocalAuthorityIDCodeList)
                 {
-                    AuthInfo = await GetEstablistmentStars(AuthInfo, client, Res, AuthResponse, authID);
+                    //API here to get each establishment 
+                    HttpResponseMessage ResEstablishment = await client.GetAsync("Establishments?localAuthorityId=760");
 
+                    //Checking the response is successful or not which is sent using HttpClient
+                    if (ResEstablishment.IsSuccessStatusCode)
+                    {
+
+                        var starResponse = ResEstablishment.Content.ReadAsStringAsync().Result;
+
+                        var starArray = (Root)Newtonsoft.Json.JsonConvert.DeserializeObject(starResponse, typeof(Root));
+
+                        AuthInfo = starArray.Authorities;
+
+                        //StarExcept = AuthInfo.Select(o => o.LocalAuthorityIdCode).Where(x => x.ratingKey != 5 || x.ratingKey != 4 || x.ratingKey != 3 || x.ratingKey != 2 || x.ratingKey != 1).Count();
+                        //Star5 += AuthInfo.Select(o => o.LocalAuthorityIdCode).Where(x => x.ratingKey = 5).Count();
+                        //Star4 += AuthInfo.Select(o => o.LocalAuthorityIdCode).Where(x => x.ratingKey = 4).Count();
+                        //Star3 += AuthInfo.Select(o => o.LocalAuthorityIdCode).Where(x => x.ratingKey = 3).Count();
+                        //Star2 += AuthInfo.Select(o => o.LocalAuthorityIdCode).Where(x => x.ratingKey = 2).Count();
+                        //Star1 += AuthInfo.Select(o => o.LocalAuthorityIdCode).Where(x => x.ratingKey = 1).Count(); 
+                    }
                 }
 
                 //Get the API for each Authority based on the ID
@@ -62,29 +86,6 @@ namespace FoodHygieneMVC.Controllers
             return View(AuthInfo);
         }
 
-        private static async Task<List<Authority>> GetEstablistmentStars(List<Authority> AuthInfo, HttpClient client, HttpResponseMessage Res, string AuthResponse, string authID)
-        {
-
-            var Star5 = 0;
-            var Star4 = 0;
-            var Star3 = 0;
-            var Star2 = 0;
-            var Star1 = 0;
-            var StarEx = 0;
-
-            //API here to get each establishment 
-            HttpResponseMessage ResEstablishment = await client.GetAsync("Establiment/{" + authID + "}");
-
-            //Checking the response is successful or not which is sent using HttpClient
-            if (ResEstablishment.IsSuccessStatusCode)
-
-                //Storing the response details recieved from web api
-                var AuthResponse = Res.Content.ReadAsStringAsync().Result;
-
-            var dataArray = (Root)Newtonsoft.Json.JsonConvert.DeserializeObject(AuthResponse, typeof(Root));
-
-            AuthInfo = dataArray.Authorities;
-            return AuthInfo;
-        }
+       
     }
 }
