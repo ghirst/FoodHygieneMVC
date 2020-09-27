e37242102ca6bdb876a736707898d74e8a71da82
Indy <hirst.geoffrey@gmail.com>
2020-09-27 18:42:13 +0100

    Um URL 404 error URL direct into browser works Recheck right URL as for Regions might need amend

--- FoodHygieneMVC/Controllers/HomeController.cs
+++ FoodHygieneMVC/Controllers/HomeController.cs
@@ -12,36 +12,36 @@ namespace FoodHygieneMVC.Controllers
     public class HomeController : Controller
     {
         //Hosted web API REST Service base url  
-        readonly string Baseurl = "http://api.ratings.food.gov.uk";
+        readonly string Baseurl = "https://api.ratings.food.gov.uk";
         public async Task<ActionResult> Index()
         {
             List<Authorities> AuthInfo = new List<Authorities>();
 
-            using (var client = new HttpClient())
+            using var client = new HttpClient
             {
                 //Passing service base url  
-                client.BaseAddress = new Uri(Baseurl);
+                BaseAddress = new Uri(Baseurl)
+            };
 
-                client.DefaultRequestHeaders.Clear();
-                //Define request data format  
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Clear();
+            //Define request data format  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
-                HttpResponseMessage Res = await client.GetAsync("api/Regions/basic");
+            //Sending request to find web api REST service resource GETRegions using HttpClient  
+            HttpResponseMessage Res = await client.GetAsync("Regions/basic");
 
-                //Checking the response is successful or not which is sent using HttpClient  
-                if (Res.IsSuccessStatusCode)
-                {
-                    //Storing the response details recieved from web api   
-                    var AuthResponse = Res.Content.ReadAsStringAsync().Result;
+            //Checking the response is successful or not which is sent using HttpClient  
+            if (Res.IsSuccessStatusCode)
+            {
+                //Storing the response details recieved from web api   
+                var AuthResponse = Res.Content.ReadAsStringAsync().Result;
 
-                    //Deserializing the response recieved from web api and storing into the Employee list  
-                    AuthInfo = JsonConvert.DeserializeObject<List<Authorities>>(AuthResponse);
+                //Deserializing the response recieved from web api and storing into the Employee list  
+                AuthInfo = JsonConvert.DeserializeObject<List<Authorities>>(AuthResponse);
 
-                }
-                //returning the employee list to view  
-                return View(AuthInfo);
             }
+            //returning the employee list to view  
+            return View(AuthInfo);
         }
     }
 }
