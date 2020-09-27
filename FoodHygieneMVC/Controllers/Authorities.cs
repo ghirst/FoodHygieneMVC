b99e713d5da635b098cc0dec81b249f4d2cc5ee6
Indy <hirst.geoffrey@gmail.com>
2020-09-27 18:07:08 +0100

    Authorities Class

---
+++ FoodHygieneMVC/Controllers/Authorities.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodHygieneMVC.Controllers
+{
+    public class Authorities
+    {
+        public int LocalAuthorityId { get; set; }
+
+        public string FriendlyName { get; set; }
+
+    }
+}
