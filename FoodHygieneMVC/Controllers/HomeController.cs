4691a3c9ce972d2b5cc2bcaf93f66e2e89076be1
Indy <hirst.geoffrey@gmail.com>
2020-09-27 18:22:37 +0100

    Typos and folder rejig To fix - URL

--- FoodHygieneMVC/Controllers/HomeController.cs
+++ FoodHygieneMVC/Controllers/HomeController.cs
@@ -1,37 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FoodHygieneMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using FoodHygieneMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks; 
 
 namespace FoodHygieneMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //Hosted web API REST Service base url  
+        readonly string Baseurl = "https://ratings.food.gov.uk/&GET=Establishments/basic/";
+        public async Task<ActionResult> Index()
         {
-            _logger = logger;
-        }
+            List<Authorities> EmpInfo = new List<Authorities>();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Employee/GetAllEmployees");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    EmpInfo = JsonConvert.DeserializeObject<List<Authorities>>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return View(EmpInfo);
+            }
         }
     }
 }
